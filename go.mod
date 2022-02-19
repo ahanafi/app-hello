552b69e2cf3567da8262e37cc71a109e9bedca60
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-02-19 08:11:39 +0700

    Upgrade dependency of golang modules

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ahanafi/app-hello
 
 go 1.17
 
-require github.com/ahanafi/hello-go v1.1.0 // indirect
+require github.com/ahanafi/hello-go/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	hello_go "github.com/ahanafi/hello-go"
+	hello_go "github.com/ahanafi/hello-go/v2"
 )
 
 func main() {
-	hello := hello_go.SayHello()
+	hello := hello_go.SayHello("Hanafi!")
 	fmt.Println(hello)
 }
